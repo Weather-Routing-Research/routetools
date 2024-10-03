08ebd743c40308d4eb7a25af84622c067dada068
daniprec <dprecioso@faculty.ie.edu>
2024-10-03 09:35:52 +0000

    jit the vectorfields

--- demo/vectorfield.py
+++ demo/vectorfield.py
@@ -1,9 +1,18 @@
 import jax.numpy as jnp
+from jax import jit
 
 
+@jit
 def vectorfield_fourvortices(
     x: jnp.ndarray, y: jnp.ndarray
 ) -> tuple[jnp.ndarray, jnp.ndarray]:
+    """
+    Vector field with four vortices.
+
+    Source:
+    https://doi.org/10.1016/j.ifacol.2021.11.097
+    """
+
     def Ru(a, b):
         return 1 / (3 * ((x - a) ** 2 + (y - b) ** 2) + 1) * -(y - b)
 
@@ -16,9 +25,16 @@ def vectorfield_fourvortices(
     return u, v
 
 
+@jit
 def vectorfield_swirlys(
     x: jnp.ndarray, y: jnp.ndarray
 ) -> tuple[jnp.ndarray, jnp.ndarray]:
+    """
+    Vector field with periodic behaviour.
+
+    Source:
+    https://doi.org/10.1016/j.ifacol.2021.11.097
+    """
     u = jnp.cos(2 * x - y - 6)
     v = 2 / 3 * jnp.sin(y) + x - 3
     return u, v
