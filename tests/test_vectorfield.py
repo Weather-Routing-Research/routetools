466c16eb61072e764ffb64c9b27be0d6cc32ee66
daniprec <dprecioso@faculty.ie.edu>
2024-10-03 10:33:25 +0000

    Add tests

---
+++ tests/test_vectorfield.py
@@ -0,0 +1,22 @@
+import jax.numpy as jnp
+import pytest
+
+from demo.vectorfield import vectorfield_fourvortices
+
+
+@pytest.mark.parametrize(
+    "x, y, u_expected,v_expected",
+    [
+        (0, 0, -0.2812, 0.1371),
+        (1, 1, -0.4663, 0.2295),
+    ],
+)
+def test_vectorfield_fourvortices(x, y, u_expected, v_expected):
+    """
+    Test the vectorfield_fourvortices function.
+    """
+    x, y = jnp.array(x), jnp.array(y)
+    u_expected, v_expected = jnp.array(u_expected), jnp.array(v_expected)
+    u, v = vectorfield_fourvortices(x, y)
+    assert jnp.allclose(u, u_expected, rtol=1e-3)
+    assert jnp.allclose(v, v_expected, rtol=1e-3)
