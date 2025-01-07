864daec3b956cd46fcfc944c0ebd103bd6cf0c8b
daniprec <dprecioso@faculty.ie.edu>
2025-01-07 11:17:05 +0000

    Normalize water level

--- routetools/land.py
+++ routetools/land.py
@@ -9,7 +9,7 @@ from scipy.ndimage import map_coordinates
 def generate_land_array(
     x: jnp.ndarray,
     y: jnp.ndarray,
-    water_level: float | None = None,
+    water_level: float = 0.4,
     resolution: tuple[int, int] | None = None,
     random_seed: int | None = None,
 ) -> jnp.ndarray:
@@ -65,17 +65,17 @@ def generate_land_array(
 
     # Generate land
     land = pn2d((len(x), len(y)), res=resolution)
-
-    # Set water level
-    if water_level is None:
-        water_level = 0.75 * np.max(land.flatten(), axis=0).astype(float)
+    # Normalize land between 0 and 1
+    land = (land - np.min(land)) / (np.max(land) - np.min(land))
 
     # Return land mask
-    return jnp.array((land > water_level).astype(int))
+    return jnp.array((land >= water_level).astype(int))
 
 
 def check_land_array(
-    curve: jnp.ndarray, land_matrix: jnp.ndarray, limits: tuple[float] | None = None
+    curve: jnp.ndarray,
+    land_matrix: jnp.ndarray,
+    limits: tuple[float, float, float, float] | None = None,
 ) -> jnp.ndarray:
     """
     Check if points on a curve are on land using bilinear interpolation.
@@ -108,7 +108,7 @@ def check_land_array(
 def generate_land_function(
     x: jnp.ndarray,
     y: jnp.ndarray,
-    water_level: float | None = None,
+    water_level: float = 0.4,
     resolution: tuple[int, int] | None = None,
     random_seed: int | None = None,
 ) -> Callable[[jnp.ndarray], jnp.ndarray]:
@@ -136,7 +136,12 @@ def generate_land_function(
     land_array = generate_land_array(
         x, y, water_level=water_level, resolution=resolution, random_seed=random_seed
     )
-    limits = (jnp.min(x), jnp.max(x), jnp.min(y), jnp.max(y))
+    limits = (
+        float(jnp.min(x)),
+        float(jnp.max(x)),
+        float(jnp.min(y)),
+        float(jnp.max(y)),
+    )
 
     def check_land(curve: jnp.ndarray) -> jnp.ndarray:
         return check_land_array(curve, land_array, limits=limits)
--- tests/test_land.py
+++ tests/test_land.py
@@ -4,5 +4,18 @@ from routetools.land import generate_land_array
 
 
 def test_generate_land_array():
-    land_array = generate_land_array(jnp.array([0, 1, 2, 3]), jnp.array([0, 1, 2, 3]))
-    assert land_array.shape == (4, 4)
+    x = jnp.linspace(0, 10, 100)
+    land_array = generate_land_array(x, x, random_seed=1)
+    assert land_array.shape == (100, 100)
+    assert land_array.max() == 1
+    assert land_array.min() == 0
+
+
+def test_water_level():
+    x = jnp.linspace(0, 10, 100)
+    # When the water level is at 0, the land array should be all land
+    land_array = generate_land_array(x, x, water_level=0, random_seed=1)
+    assert land_array.min() == 1
+    # When the water level is over 1, the land array should be all water
+    land_array = generate_land_array(x, x, water_level=1.1, random_seed=1)
+    assert land_array.max() == 0
