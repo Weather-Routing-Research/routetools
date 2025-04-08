d9cc91dbae5f0adc988d427d0104c15e2642fde2
daniprec <dprecioso@faculty.ie.edu>
2025-04-08 16:31:03 +0200

    Implement for loop using lax

--- routetools/cost.py
+++ routetools/cost.py
@@ -2,7 +2,7 @@ from collections.abc import Callable
 from functools import partial
 
 import jax.numpy as jnp
-from jax import jit
+from jax import jit, lax
 
 
 @partial(jit, static_argnums=(0, 2, 3, 4))
@@ -152,22 +152,31 @@ def cost_function_constant_speed_time_variant(
     # Power of the speed through water
     v2 = travel_stw**2
 
-    # Initialize times as an array of size B
-    t = jnp.zeros(curve.shape[0])
-    # Move along the curve one point at a time (dimension L)
-    for i in range(curve.shape[1] - 1):
+    def step(
+        t: jnp.ndarray,
+        inputs: tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray],
+    ) -> tuple[jnp.ndarray, jnp.ndarray]:
+        x, y, dx_step, dy_step, d2_step = inputs
         # When sailing from i-1 to i, we interpolate the vector field at the midpoint
-        uinterp, vinterp = vectorfield(curvex[:, i], curvey[:, i], t)
+        uinterp, vinterp = vectorfield(x, y, t)
         # Power of the current speed
         w2 = uinterp**2 + vinterp**2
-        dw = dx[:, i] * uinterp + dy[:, i] * vinterp
+        dw = dx_step * uinterp + dy_step * vinterp
         # Cost is the time to travel the segment
-        dt = jnp.sqrt(d2[:, i] / (v2 - w2) + dw**2 / (v2 - w2) ** 2) - dw / (v2 - w2)
+        dt = jnp.sqrt(d2_step / (v2 - w2) + dw**2 / (v2 - w2) ** 2) - dw / (v2 - w2)
         # Current > speed -> infeasible path
         dt = jnp.where(v2 <= w2, 1e10, dt)
         # Update the times
-        t = t + dt
-    return t
+        return t + dt, dt
+
+    # Initialize inputs for the JAX-native looping construct
+    inputs = (curvex.T, curvey.T, dx.T, dy.T, d2.T)
+    t_init = jnp.zeros(curve.shape[0])
+
+    # Use lax to implement the for loop
+    t_final, dt_array = lax.scan(step, t_init, inputs)  
+
+    return t_final
 
 
 @partial(jit, static_argnums=(0, 2))
